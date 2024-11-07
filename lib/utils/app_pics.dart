ff30ff06f82404f468b3dcc1313ec8b25e89f30f
Amira Tarek <amirat737@gmail.com>
2024-11-07 02:25:43 +0200

    add app pics

---
+++ lib/utils/app_pics.dart
@@ -0,0 +1,32 @@
+abstract class Assets {
+  static const kPicsAvatar1 = "assets/pics/avatar_1.svg";
+
+  static const kPicsAvatar2 = "assets/pics/avatar_2.svg";
+
+  static const kPicsAvatar3 = "assets/pics/avatar_3.svg";
+
+  static const kPicsBalance = "assets/pics/balance.svg";
+
+  static const kPicsCardBackground = "assets/pics/card_background.png";
+
+  static const kPicsDashboard = "assets/pics/dashboard.svg";
+
+  static const kPicsExpenses = "assets/pics/expenses.svg";
+
+  static const kPicsGallery = "assets/pics/gallery.svg";
+
+  static const kPicsIncome = "assets/pics/income.svg";
+
+  static const kPicsInvestments = "assets/pics/investments.svg";
+
+  static const kPicsLogout = "assets/pics/logout.svg";
+
+  static const kPicsSettings = "assets/pics/settings.svg";
+
+  static const kPicsStatistics = "assets/pics/statistics.svg";
+
+  static const kPicsTransctions = "assets/pics/transctions.svg";
+
+  static const kPicsWallet = "assets/pics/wallet.svg";
+}
+
