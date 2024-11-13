fc8e613f88fa3289e0b05b63487e3f6fd458390a
Amira Tarek <amirat737@gmail.com>
2024-11-13 22:49:10 +0200

    create colors pallete

---
+++ lib/utils/colors_pallete.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsPallete {
+  static const kPrimaryColor = Color(0xff064061);
+  static const kWhite = Color(0xffFFFFFF);
+  static const kBlueSky = Color(0xff4EB7F2);
+  static const kWhiteSmoke = Color(0xffFAFAFA);
+  static const kGrayLight = Color(0xffAAAAAA);
+  static const kGrayWhisper = Color(0xffF1F1F1);
+  static const kGrayOnyx = Color(0xff292D32);
+  static const kPinkSalmon = Color(0xffF3735E);
+  static const kBlueMedium = Color(0xff208CC8);
+  static const kBlack = Color(0xff000000);
+  static const kGrayGainsboro = Color(0xffE8E8E8);
+  static const kGreenMint = Color(0xff7DD97B);
+  static const kBeige = Color(0xffE2DECD);
+  static const kWhiteOff = Color(0xffF7F9FA);
+}
