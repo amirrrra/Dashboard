d2269541961deb32c351ee63cea384582c5ec637
Amira Tarek <amirat737@gmail.com>
2024-11-14 02:38:13 +0200

    create desktop layout widget

---
+++ lib/views/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:dashboard/widgets/desktop_layout_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DesktopLayoutWidget()),
+    );
+  }
+}
