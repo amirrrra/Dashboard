bb6d7d757de336d929a71c89fd4564e2537d8f92
Amira Tarek <amirat737@gmail.com>
2024-11-18 01:59:34 +0200

    build transaction header widget

--- lib/widgets/transaction_widget.dart
+++ lib/widgets/transaction_widget.dart
@@ -1,3 +1,6 @@
+import 'package:dashboard/utils/app_styles.dart';
+import 'package:dashboard/utils/colors_pallete.dart';
+import 'package:dashboard/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
 
 class TransactionWidget extends StatelessWidget {
@@ -5,6 +8,26 @@ class TransactionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        HeaderWidget(
+          title: 'Transaction History',
+          widget: Text(
+            'See all',
+            style: AppStyles.medium16(context).copyWith(
+              color: ColorsPallete.kBlueSky,
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          '13 April 2022',
+          style: AppStyles.medium16(context).copyWith(
+            color: ColorsPallete.kGrayLight,
+          ),
+        ),
+      ],
+    );
   }
 }
